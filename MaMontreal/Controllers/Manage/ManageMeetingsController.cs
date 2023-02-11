00648d642f34457f615daa81f79004325bd9c648
Aftab Safdar <safdar_aftab@hotmail.com>
2023-02-10 21:55:35 -0500

    Fixed ManageMeetingCreateEdit -> no flashMessage for image errors

--- MaMontreal/Controllers/Manage/ManageMeetingsController.cs
+++ MaMontreal/Controllers/Manage/ManageMeetingsController.cs
@@ -112,11 +112,13 @@ namespace MaMontreal.Controllers_Manage
 
             if (meeting._ImageFile != null && meeting._ImageFile?.Length > 250000)
             {
+                TempData["flashMessage"] = JsonConvert.SerializeObject(new FlashMessage("Image file size cannot exceed 250KB", "danger"));
                 ModelState.AddModelError("_ImageFile", "Image file size cannot exceed 250KB");
                 return View(meeting);
             }
             else if (meeting._ImageFile?.ContentType.ToLower().Substring(0, 5).Equals("image") == false)
             {
+                TempData["flashMessage"] = JsonConvert.SerializeObject(new FlashMessage("The file you uploaded is not an image file", "danger"));
                 ModelState.AddModelError("_ImageFile", "The file you uploaded is not an image file");
                 return View(meeting);
             }
@@ -209,13 +211,15 @@ namespace MaMontreal.Controllers_Manage
             ViewBag.Languages = _context.Languages.ToList<Language>();
             ViewData["Gsrs"] = _userService?.GetUsersWithRole("gsr").Result;
 
-            if (meeting._ImageFile != null && meeting._ImageFile?.Length > 250000)
+            if (meeting._ImageFile?.Length > 250000)
             {
+                TempData["flashMessage"] = JsonConvert.SerializeObject(new FlashMessage("Image file size cannot exceed 250KB", "danger"));
                 ModelState.AddModelError("_ImageFile", "Image file size cannot exceed 250KB");
                 return View(meeting);
             }
             else if (meeting._ImageFile?.ContentType.ToLower().Substring(0, 5).Equals("image") == false)
             {
+                TempData["flashMessage"] = JsonConvert.SerializeObject(new FlashMessage("The file you uploaded is not an image file", "danger"));
                 ModelState.AddModelError("_ImageFile", "The file you uploaded is not an image file");
                 return View(meeting);
             }
