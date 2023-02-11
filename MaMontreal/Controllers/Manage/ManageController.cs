58ff9bc59347158bf505cdc6c701dbbf295e8da9
juliatag <julieta.jabbot@gmail.com>
2023-02-10 19:57:27 -0500

    updating Dasboard layout

--- MaMontreal/Controllers/Manage/ManageController.cs
+++ MaMontreal/Controllers/Manage/ManageController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MaMontreal.Data;
+using MaMontreal.Models.NotMapped;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace MaMontreal.Controllers.Manage
 {
@@ -24,6 +26,28 @@ namespace MaMontreal.Controllers.Manage
 
         [Route("")]
         public IActionResult Index()
+        {
+            if (User.IsInRole("admin"))
+                return RedirectToAction("AdminDash");
+            if (User.IsInRole("gsr"))
+                return RedirectToAction("GsrDash");
+            return RedirectToAction("MemberDash");
+        }
+
+        [Route("Member")]
+        public IActionResult MemberDash()
+        {
+            return View();
+        }
+
+        [Route("Admin")]
+        public IActionResult AdminDash()
+        {
+            return View();
+        }
+
+        [Route("Gsr")]
+        public IActionResult GsrDash()
         {
             return View();
         }
